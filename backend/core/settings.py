c9b275ef4c9161f608a062fb9e64dca295442ba7
Arjun8124 <gupta.arjun.8124@gmail.com>
2026-06-19 18:24:51 +0530

    Update settings.py to configure allowed hosts and add CORS settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f=u%xc2y8@&9y8(yt2hu771btpr)e_bb)y)@pl!%%e%re2o=$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CORS_ALLOW_ALL_ORIGINS = True
+DATA_UPLOAD_MAX_MEMORY_SIZE = 20 * 1024 * 1024
